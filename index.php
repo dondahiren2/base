d0392be4f014f4dee9588885b362bdfd2801090b
dondahiren2 <dondahiren2@gmail.com>
2018-09-26 20:16:47 +0530

    asd
    asdas
    
    Co-Authored-By: PKINFOSYS2018 <pkinfosys2018@users.noreply.github.com>

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php 
 
 $path = 'https://touchalife.s3-ap-southeast-1.amazonaws.com/uploads/STD/STD_100920181308089937/logo/purvesh-tejani-20180910132008951.png';
 $type = pathinfo($path, PATHINFO_EXTENSION);
@@ -5,6 +6,7 @@ $data = file_get_contents($path);
 $base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
 
 echo ".... ". $data . " .... ";
-
-
 echo $base64;
+ //s ass
+//asd asd
+?>
