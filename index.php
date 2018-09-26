827cc096ec039c22ea25b89e96aa224b5a9ccf90
dondahiren2 <dondahiren2@gmail.com>
2018-09-26 20:05:04 +0530

    add new file
    convert image to base64

---
+++ index.php
@@ -0,0 +1,10 @@
+
+$path = 'https://touchalife.s3-ap-southeast-1.amazonaws.com/uploads/STD/STD_100920181308089937/logo/purvesh-tejani-20180910132008951.png';
+$type = pathinfo($path, PATHINFO_EXTENSION);
+$data = file_get_contents($path);
+$base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
+
+echo ".... ". $data . " .... ";
+
+
+echo $base64;
